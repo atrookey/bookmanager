46b16920b07db9048fc4a1078856bd129fbb4e88
Alexander Rookey <atrookey@Alexanders-MacBook-Pro.local>
2016-03-19 21:12:58 -0400

    finished building flask boilerplate.  Never again

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/')
+def home():
+  return render_template('pages/home.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+  return render_template('errors/404.html'), 404
+
+if not app.debug:
+  file_handler = FileHandler('error.log')
+  file_handler.setFormatter(
+      Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+  )
+  app.logger.setLevel(logging.INFO)
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
+  app.logger.info('errors')
+
+if __name__ == '__main__':
+  app.run()
